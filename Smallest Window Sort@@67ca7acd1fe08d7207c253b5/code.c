aa7458a1d8905001f57e688cb455e2663a3f8ec2
JasleenKaur2508 <hiyajasleen@gmail.com>
2025-03-21 19:04:41 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,55 +1,48 @@
 #include <stdio.h>
 
-int findUnsortedSubarray(int arr[], int n) {
-    int left = 0, right = n - 1;
-
-    // Find first out-of-order index from left
-    while (left < n - 1 && arr[left] <= arr[left + 1]) {
-        left++;
+int findUnsortedSubarrayLength(int arr[], int n) {
+    int start = -1, end = -1;
+    // Finding the initial boundaries
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            start = i;
+            break;
+        }
     }
-    if (left == n - 1) {
-        return 0; // Already sorted
+    for (int i = n - 1; i > 0; i--) {
+        if (arr[i] < arr[i - 1]) {
+            end = i;
+            break;
+        }
     }
+    // If the array is already sorted
+    if (start == -1) return 0;
 
-    // Find first out-of-order index from right
-    while (right > 0 && arr[right] >= arr[right - 1]) {
-        right--;
-    }
-
-    // Find min and max in the unsorted subarray
-    int min_val = arr[left], max_val = arr[left];
-    for (int i = left; i <= right; i++) {
-        if (arr[i] < min_val) min_val = arr[i];
-        if (arr[i] > max_val) max_val = arr[i];
-    }
-
-    // Expand left boundary
-    while (left > 0 && arr[left - 1] > min_val) {
-        left--;
+    // Finding the min and max in the unsorted subarray
+    int minVal = arr[start], maxVal = arr[start];
+    for (int i = start; i <= end; i++) {
+        if (arr[i] < minVal) minVal = arr[i];
+        if (arr[i] > maxVal) maxVal = arr[i];
     }
 
-    // Expand right boundary
-    while (right < n - 1 && arr[right + 1] < max_val) {
-        right++;
-    }
+    // Expanding the boundaries
+    while (start > 0 && arr[start - 1] > minVal) start--;
+    while (end < n - 1 && arr[end + 1] < maxVal) end++;
 
-    return right - left + 1;
+    return end - start + 1;
 }
 
 int main() {
-    int T;
-    scanf("%d", &T);
-
-    while (T--) {
-        int N;
-        scanf("%d", &N);
-        int arr[N];
-
-        for (int i = 0; i < N; i++) {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int i = 0; i < n; i++) {
             scanf("%d", &arr[i]);
         }
-
-        printf("%d\n", findUnsortedSubarray(arr, N));
+        printf("%d\n", findUnsortedSubarrayLength(arr, n));
     }
     return 0;
-}    
+}
