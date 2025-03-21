4c70d4ba1e859386d074066434f128f5fab53b5f
JasleenKaur2508 <hiyajasleen@gmail.com>
2025-03-21 19:26:54 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>  // Include this for strcmp and strcpy
 
 // Function to sort an array of strings using bubble sort
 void bubbleSort(char arr[][100], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (strcmp(arr[j], arr[j + 1]) > 0) {
-                // Swap the strings if they are in the wrong order
+                // Swap the strings
                 char temp[100];
                 strcpy(temp, arr[j]);
                 strcpy(arr[j], arr[j + 1]);
@@ -14,4 +14,29 @@ void bubbleSort(char arr[][100], int n) {
             }
         }
     }
+}
+
+// Function to print the array of strings
+void printArray(char arr[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", arr[i]);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of strings: ");
+    scanf("%d", &n);
+    char arr[n][100];
+
+    printf("Enter the strings:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%s", arr[i]);
+    }
+
+    bubbleSort(arr, n);
+    printf("\nSorted strings:\n");
+    printArray(arr, n);
+
+    return 0;
 }
