d0bfe9af7dbb619a11f2299b4bdd3952ba32584c
JasleenKaur2508 <hiyajasleen@gmail.com>
2025-03-22 10:23:04 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,39 +1,25 @@
 #include <stdio.h>
-#include <string.h>  // For strcmp and strcpy
+#include <string.h>
+#include <stdlib.h>
 
-void bubbleSort(char arr[][100], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (strcmp(arr[j], arr[j + 1]) > 0) {
-                char temp[100];
-                strcpy(temp, arr[j]);
-                strcpy(arr[j], arr[j + 1]);
-                strcpy(arr[j + 1], temp);
-            }
-        }
-    }
-}
-
-void printArray(char arr[][100], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", arr[i]);
-    }
+int compareStrings(const void *a, const void *b) {
+    return strcmp((const char *)a, (const char *)b);
 }
 
 int main() {
     int n;
-    printf("Enter the number of strings: ");
     scanf("%d", &n);
     char arr[n][100];
 
-    printf("Enter the strings:\n");
     for (int i = 0; i < n; i++) {
         scanf("%s", arr[i]);
     }
 
-    bubbleSort(arr, n);  // Sorting function
-    printf("\nSorted strings:\n");
-    printArray(arr, n);  // Printing function
+    qsort(arr, n, sizeof(arr[0]), compareStrings);
+
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", arr[i]);
+    }
 
     return 0;
 }
